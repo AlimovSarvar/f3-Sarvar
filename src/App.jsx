f4995441d4ff55682b5083feb18071f55ac5df8f
AlimovSarvar <alimovsarvar12.08.2008@gmail.com>
2023-08-08 18:30:27 +0500

    ozgarish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.scss';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Row from './components/Row/Row';
-import Best from './components/Best/Best';
 import Choose from './components/Choose/Choose';
 import Custom from './components/Custom/Custom';
 import Canada from './components/Canada/Canada';
@@ -17,7 +16,7 @@ function App() {
     <Header/>
     <Hero/>
     <Row/>
-    <Best/>
+    
     <Choose/>
     <Custom/>
     <Canada/>
