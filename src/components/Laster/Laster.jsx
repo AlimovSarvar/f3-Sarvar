78722424e559f13f2b8d2e1206be396161880892
ziyo2007 <qodirjonovz6@gmail.com>
2023-08-15 13:00:51 +0300

    laster ozgarish

--- src/components/Laster/Laster.jsx
+++ src/components/Laster/Laster.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import leftLaster from '../../assets/img/left-laster.png'
+import { Link } from 'react-router-dom'
+import './Laster.scss'
 
 function Laster() {
   return (
-    <div>
-      
+    <div className='laster'>
+          <h4 className='laster__tittle'>Lastest News and Resources</h4>
+          <p className='totc'>See the developments that have occurred to TOTC in the world</p> 
+          <div className="left__laster">
+            <div>
+            <img src={leftLaster} alt="" />
+            </div>
+            <div>
+            <span>NEWS</span>
+            <h5>Class adds $30 million to its balance sheet for a Zoom-friendly edtech solution</h5>
+            <p>Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...</p>
+            <Link>Read more</Link>
+            </div>
+            </div>   
     </div>
   )
 }
