7708ad2f50470033035829c7bb60f2f83dd447e1
ziyo2007 <qodirjonovz6@gmail.com>
2023-08-17 14:14:37 +0300

    laster-ozgarish

--- src/components/Laster/Laster.jsx
+++ src/components/Laster/Laster.jsx
@@ -1,24 +1,51 @@
 import React from 'react'
 import leftLaster from '../../assets/img/left-laster.png'
+import classLaster from '../../assets/img/class-laster.png'
+import zoomLaster from '../../assets/img/zoom-laster.png'
+import formerLaster from '../../assets/img/former-laster.png'
 import { Link } from 'react-router-dom'
 import './Laster.scss'
 
 function Laster() {
   return (
-    <div className='laster'>
+    <div className='laster container'>
           <h4 className='laster__tittle'>Lastest News and Resources</h4>
           <p className='totc'>See the developments that have occurred to TOTC in the world</p> 
-          <div className="left__laster">
+        <div className="inner">
+        <div className="left__laster">            
             <div>
-            <img src={leftLaster} alt="" />
-            </div>
+            <img src={leftLaster} alt="" /> 
+              </div>          
             <div>
-            <span>NEWS</span>
-            <h5>Class adds $30 million to its balance sheet for a Zoom-friendly edtech solution</h5>
-            <p>Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...</p>
-            <Link>Read more</Link>
+            <span className='news'>NEWS</span>
+            <h5 className='info'>Class adds $30 million to its balance sheet for <br /> a Zoom-friendly edtech solution</h5>
+            <p className='laster__P'>Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...</p>
+            <span className='link'>
+              <a className='laster__link' href="#">Read more</a>
+            </span> 
+              </div>          
             </div>
+            <div className="right__laster">
+                 <ul className='laster__list'>
+                  <li className='laster__item'>
+                    <img className='first__img' src={classLaster} alt="" />
+                    <h5 className='first__tittle'>Class Technologies Inc. Closes $30 Million <br /> Series A Financing to Meet High Demand</h5>
+                    <p className='first__info'>Class Technologies Inc., the company that <br /> created Class,...</p>
+                  </li>
+                  <li className='laster__item'>
+                    <img className='first__img' src={zoomLaster} alt="" />
+                    <h5 className='first__tittle'>Class Technologies Inc. Closes $30 Million <br /> Series A Financing to Meet High Demand</h5>
+                    <p className='first__info'>Class Technologies Inc., the company that <br /> created Class,...</p>
+                  </li>
+                  <li className='laster__item'>
+                    <img className='first__img' src={formerLaster} alt="" />
+                    <h5 className='first__tittle'>Class Technologies Inc. Closes $30 Million <br /> Series A Financing to Meet High Demand</h5>
+                    <p className='first__info'>Class Technologies Inc., the company that <br /> created Class,...</p>
+                  </li>
+                    
+                 </ul>
             </div>   
+        </div>
     </div>
   )
 }
