c65851c05696a0fd413606837c284fd1c7ef8d21
ziyo2007 <qodirjonovz6@gmail.com>
2023-08-17 20:32:02 +0300

    ozgarish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Footer.scss'
+import footer from '../../assets/img/footer.png'
 
 function Footer() {
   return (
-    <div>
-      
+    <div className='footer container'>
+      <img className='footer_img' src={footer} alt="" />
+      <h5 className='footer__tittle'>Virtual Class <br />   for Zoom</h5>
+      <p className='footer__p'>Subscribe to get our Newsletter</p>
+      <input className='footer__inp' type="text" placeholder='Your Email' />
+      <button className='footer__btn'>Subscribe</button>
+      <p className='footer__p__bottom'>Careers Privacy Policy Terms&Conditions</p>
+      <p className='date__p'>© 2021 Class Technologies Inc. </p>
     </div>
   )
 }
