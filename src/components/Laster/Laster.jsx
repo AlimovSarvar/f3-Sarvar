0a532d461fa1a55f5d13485573776887b0bcde69
AlimovSarvar <alimovsarvar12.08.2008@gmail.com>
2023-08-19 16:02:27 +0500

    ozgarish

--- src/components/Laster/Laster.jsx
+++ src/components/Laster/Laster.jsx
@@ -13,31 +13,31 @@ function Laster() {
         <div className="inner">
         <div className="left__laster">            
             <div>
-            <img src={leftLaster} alt="" /> 
+            <img src={leftLaster} alt="img" /> 
               </div>          
             <div>
             <span className='news'>NEWS</span>
             <h5 className='info'>Class adds $30 million to its balance sheet for <br /> a Zoom-friendly edtech solution</h5>
             <p className='laster__P'>Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...</p>
             <span className='link'>
-              <a className='laster__link' href="#">Read more</a>
+              <a className='laster__link' href="#/">Read more</a>
             </span> 
               </div>          
             </div>
             <div className="right__laster">
                  <ul className='laster__list'>
                   <li className='laster__item'>
-                    <img className='first__img' src={classLaster} alt="" />
+                    <img className='first__img' src={classLaster} alt="img" />
                     <h5 className='first__tittle'>Class Technologies Inc. Closes $30 Million <br /> Series A Financing to Meet High Demand</h5>
                     <p className='first__info'>Class Technologies Inc., the company that <br /> created Class,...</p>
                   </li>
                   <li className='laster__item'>
-                    <img className='first__img' src={zoomLaster} alt="" />
+                    <img className='first__img' src={zoomLaster} alt="img" />
                     <h5 className='first__tittle'>Class Technologies Inc. Closes $30 Million <br /> Series A Financing to Meet High Demand</h5>
                     <p className='first__info'>Class Technologies Inc., the company that <br /> created Class,...</p>
                   </li>
                   <li className='laster__item'>
-                    <img className='first__img' src={formerLaster} alt="" />
+                    <img className='first__img' src={formerLaster} alt="img" />
                     <h5 className='first__tittle'>Class Technologies Inc. Closes $30 Million <br /> Series A Financing to Meet High Demand</h5>
                     <p className='first__info'>Class Technologies Inc., the company that <br /> created Class,....</p>
                   </li>
