e1d749eeef42ea3c73352faba2d47d5b4f79d08c
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-08-22 15:07:55 +0500

    ozgarisfkf

--- src/components/Say/Say.jsx
+++ src/components/Say/Say.jsx
@@ -3,10 +3,27 @@ import React from 'react'
 
 function Say() {
   return (
-    <div className='container'>
-      
-   
-      
+    <div className='say container'>
+        <div className="say__item">
+            <div className="say__left">
+                <p>
+                    ---------    TESTIMONIAL
+                </p>
+                <h2>
+                    What They Say?
+                </h2>
+                <p>
+                TOTC has got more than 100k positive ratings from our users around the world. 
+                <br />
+                Some of the students and teachers were greatly helped by the Skilline.
+                <br />
+                Are you too? Please give your assessment
+                </p>
+                <button>
+                Write your assessment
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
