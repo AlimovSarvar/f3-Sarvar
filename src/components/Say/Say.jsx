3c2c8bf0335e48a4a0e1fe9946fa9b844f2fd4af
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-08-28 16:19:53 +0500

    hatolarni ozgartirdim

--- src/components/Say/Say.jsx
+++ src/components/Say/Say.jsx
@@ -31,10 +31,8 @@ function Say() {
           <div className="big">
           <div className="say__right__inner">
             <img src={sayRight} alt="img" />
-            
-          </div>
-          <div className='say__right__bottom'>
-              <p>
+            <span className='say__right__bottom'>
+            <p>
                 "Thank you so much for your help. It's exactly what I've been looking for. You won't regret it. It really saves me time and effort. TOTC is exactly what our business has been lacking."
               </p>
                <span className='text'>
@@ -54,8 +52,9 @@ function Say() {
                       </p>
                   </span>
                </span>
-
-            </div>
+            </span>
+          </div>
+         
           </div>
 
         </div>
